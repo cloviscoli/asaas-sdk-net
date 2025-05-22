0e6cdd6e21c95980952f36de1eeff0b712ec54d6
Clovis Coli Jr <clovis@axiontecnologia.com.br>
2025-05-22 00:40:54 -0300

    Integração com Bling

--- AsaasClient/Models/Customer/Customer.cs
+++ AsaasClient/Models/Customer/Customer.cs
@@ -2,53 +2,52 @@
 using Newtonsoft.Json;
 using System;
 
-namespace AsaasClient.Models.Customer
+namespace AsaasClient.Models.Customer;
+
+public class Customer
 {
-    public class Customer
-    {
-        public string Id { get; set; }
+    public string Id { get; set; }
 
-        public DateTime DateCreated { get; set; }
+    public DateTime DateCreated { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        public string Email { get; set; }
+    public string Email { get; set; }
 
-        public string Phone { get; set; }
+    public string Phone { get; set; }
 
-        public string MobilePhone { get; set; }
+    public string MobilePhone { get; set; }
 
-        public string CpfCnpj { get; set; }
+    public string CpfCnpj { get; set; }
 
-        public string PostalCode { get; set; }
+    public string PostalCode { get; set; }
 
-        public string Address { get; set; }
+    public string Address { get; set; }
 
-        public string AddressNumber { get; set; }
+    public string AddressNumber { get; set; }
 
-        public string Complement { get; set; }
+    public string Complement { get; set; }
 
-        public string Province { get; set; }
+    public string Province { get; set; }
 
-        public string ExternalReference { get; set; }
+    public string ExternalReference { get; set; }
 
-        public bool NotificationDisabled { get; set; }
+    public bool NotificationDisabled { get; set; }
 
-        public string AdditionalEmails { get; set; }
+    public string AdditionalEmails { get; set; }
 
-        public string MunicipalInscription { get; set; }
+    public string MunicipalInscription { get; set; }
 
-        public PersonType? PersonType { get; set; }
+    public PersonType? PersonType { get; set; }
 
-        public bool Deleted { get; set; }
+    public bool Deleted { get; set; }
 
-        [JsonProperty(PropertyName = "city")]
-        public long? CityId { get; set; }
+    [JsonProperty(PropertyName = "city")]
+    public long? CityId { get; set; }
 
-        public string State { get; set; }
+    public string State { get; set; }
 
-        public string Country { get; set; }
+    public string Country { get; set; }
 
-        public string Observations { get; set; }
-    }
+    public string Observations { get; set; }
 }
